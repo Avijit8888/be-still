b1aa1ac72959e0c1fe3246d7ec434186fece34e5
Avijit8888 <abhi.das97A@outlook.com>
2026-02-03 20:11:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,21 @@ const message=document.getElementById("message");
 const messageText=document.getElementById("messageText");
 const awakening=document.getElementById("awakening");
 const bg=document.querySelector(".bg");
-const transition=document.getElementById("transition");
 
 /* Quote Click */
 quote.addEventListener("click",()=>{
-startTransition(()=>{
+
 quote.classList.add("hidden");
 options.classList.remove("hidden");
 
 setTimeout(()=>{
 options.classList.add("show");
 },100);
-});
+
 });
 
 /* Choose Option */
 function choose(type){
-
-startTransition(()=>{
 
 options.classList.add("hidden");
 message.classList.remove("hidden");
@@ -50,13 +47,11 @@ awakening.classList.remove("hidden");
 awakening.classList.add("show");
 },6000);
 
-});
 }
 
 /* Back */
 function goBack(){
 
-startTransition(()=>{
 message.classList.add("hidden");
 options.classList.remove("hidden");
 
@@ -66,13 +61,12 @@ bg.classList.remove("zoom");
 setTimeout(()=>{
 options.classList.add("show");
 },100);
-});
+
 }
 
 /* Restart */
 function restartJourney(){
 
-startTransition(()=>{
 awakening.classList.remove("show");
 awakening.classList.add("hidden");
 
@@ -83,55 +77,5 @@ quote.classList.remove("hidden");
 
 bg.classList.remove("blur");
 bg.classList.remove("zoom");
-});
+
 }
-
-/* Transition */
-function startTransition(callback){
-transition.classList.add("active");
-
-setTimeout(()=>{
-callback();
-transition.classList.remove("active");
-},800);
-}
-
-/* STAR UNIVERSE */
-const canvas=document.getElementById("stars");
-const ctx=canvas.getContext("2d");
-
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
-
-let stars=[];
-
-for(let i=0;i<120;i++){
-stars.push({
-x:Math.random()*canvas.width,
-y:Math.random()*canvas.height,
-size:Math.random()*2,
-speed:Math.random()*0.3
-});
-}
-
-function animateStars(){
-
-ctx.clearRect(0,0,canvas.width,canvas.height);
-ctx.fillStyle="white";
-
-stars.forEach(star=>{
-ctx.beginPath();
-ctx.arc(star.x,star.y,star.size,0,Math.PI*2);
-ctx.fill();
-
-star.y+=star.speed;
-
-if(star.y>canvas.height){
-star.y=0;
-}
-});
-
-requestAnimationFrame(animateStars);
-}
-
-animateStars();
